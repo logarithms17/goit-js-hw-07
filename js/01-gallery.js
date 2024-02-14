b90a9b973498a5d4e8f314c783fca4ff9844f914
logarithms17 <142600036+logarithms17@users.noreply.github.com>
2024-02-14 23:30:57 +0800

    task 1 completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+let gallery = document.querySelector(".gallery")
+console.log(gallery)
+
+for (let galleryItem of galleryItems) {
+    // console.log(galleryItem)
+    let li = `
+    <li class="gallery__item">
+        <a class="gallery__link">
+            <img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}"/>
+        </a>
+    </li>`;
+    // gallery.appendChild(li)
+    // gallery.innerText += li;
+    gallery.innerHTML += li;
+}
+
+gallery.addEventListener("click", (e) => {
+    let originalImage = e.target.getAttribute("data-source")
+    console.log(originalImage)
+    let fullScreen = basicLightbox.create(`<img src="${originalImage}" width="800" height="600"/>`)
+    fullScreen.show();
+
+})
